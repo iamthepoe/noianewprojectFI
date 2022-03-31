635ae263942a5b72f1b38a8335faee94eb501439
Kayk Santos <80800368+KaykSantos@users.noreply.github.com>
2022-03-31 14:49:16 -0300

    Adicionando o caso 4
    Consulta por Modelo/Marca

--- parteMuckz.cs
+++ parteMuckz.cs
@@ -40,5 +40,45 @@ namespace NoiasProject
 
             Console.ReadKey();
         }
+        
+        
+        //PARTE 4 DO EXERCÍCIO - Consulta por Modelo/Marca
+        static void Caso4()
+        }
+            string ModeloMarca = "modelo/marca";
+            Console.Write("Digite o modelo/marca do veículo: ");
+            ModeloMarca = Console.ReadLine();
+
+            for (int c = 0; c <= 301; c++)
+            {
+                if (ModeloMarca == "Váriavel pública do modelo carro")
+                {
+                    Console.WriteLine($"Carro da marca: {"Váriavel publica da classe"}\nPlaca: {"Váriavel publica da classe"}\nModelo: {"Váriavel publica da classe"}\nFabricante {"Váriavel publica da classe"}\nAno: {"Váriavel publica da classe"}\nCor: {"Váriavel publica da classe"}\nNúmero de portas: {"Váriavel publica da classe"}\nNúmero de eixos: {"Váriavel publica da classe"}\nPeso máximo: {"Váriavel publica da classe"}\n Váriavel específica: {"Váriavel publica da classe"}");
+                    break;
+                }
+                else if (ModeloMarca == "Váriavel publica da marca carro")
+                {
+                    Console.WriteLine($"Carro da marca: {"Váriavel publica da classe"}\nPlaca: {"Váriavel publica da classe"}\nModelo: {"Váriavel publica da classe"}\nFabricante {"Váriavel publica da classe"}\nAno: {"Váriavel publica da classe"}\nCor: {"Váriavel publica da classe"}\nNúmero de portas: {"Váriavel publica da classe"}\nNúmero de eixos: {"Váriavel publica da classe"}\nPeso máximo: {"Váriavel publica da classe"}\n Váriavel específica: {"Váriavel publica da classe"}");
+                    break;
+                }
+            }
+            for (int c = 0; c <= 201; c++)
+            {
+                if (ModeloMarca == "Váriavel pública do modelo caminhão")
+                {
+                    Console.WriteLine($"Caminhão da marca: {"Váriavel publica da classe"}\nPlaca: {"Váriavel publica da classe"}\nModelo: {"Váriavel publica da classe"}\nFabricante {"Váriavel publica da classe"}\nAno: {"Váriavel publica da classe"}\nCor: {"Váriavel publica da classe"}\nNúmero de portas: {"Váriavel publica da classe"}\nNúmero de eixos: {"Váriavel publica da classe"}\nPeso máximo: {"Váriavel publica da classe"}\n Váriavel específica: {"Váriavel publica da classe"}");
+                    break;
+                }
+                else if (ModeloMarca == "Váriavel publica da marca caminhão")
+                {
+                    Console.WriteLine($"Caminhão da marca: {"Váriavel publica da classe"}\nPlaca: {"Váriavel publica da classe"}\nModelo: {"Váriavel publica da classe"}\nFabricante {"Váriavel publica da classe"}\nAno: {"Váriavel publica da classe"}\nCor: {"Váriavel publica da classe"}\nNúmero de portas: {"Váriavel publica da classe"}\nNúmero de eixos: {"Váriavel publica da classe"}\nPeso máximo: {"Váriavel publica da classe"}\n Váriavel específica: {"Váriavel publica da classe"}");
+                    break;
+                }
+            }
+
+            ModeloMarca = "modelo/marca";
+
+            Console.ReadKey();
+        }
     }
 }
