eb0244b4648ee249f8449ca3b40dd17764a36ab8
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-03-31 13:56:25 -0300

    uploading a new class

---
+++ NoiasProject/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiasProject
+{
+    class Veiculo
+    {
+        public string modelo;
+        public string fabricante;
+        public int ano;
+        public string cor;
+        public int numero_portas;
+
+        public Veiculo(string modelo, string fabricante, int ano, string cor, int numero_portas)
+        {
+            this.modelo = modelo;
+            this.fabricante = fabricante;
+            this.ano = ano;
+            this.cor = cor;
+            this.numero_portas = numero_portas;
+        }
+
+    }
+}
