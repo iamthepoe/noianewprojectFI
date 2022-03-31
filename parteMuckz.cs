4aca19221354bf8cca61dfa1f059dba39f44d555
Kayk Santos <80800368+KaykSantos@users.noreply.github.com>
2022-03-31 14:56:39 -0300

    Adicionando o caso 7
    Exibir todos os caminhões cadastrados

--- parteMuckz.cs
+++ parteMuckz.cs
@@ -14,37 +14,8 @@ namespace NoiasProject
     }
         static void Main(string[] args)
         {
-            //PARTE 3 DO EXERCÍCIO - Consulta por placa
+            //PARTE 4 DO EXERCÍCIO - Consulta por Modelo/Marca
 
-            string placa = "placa";
-            Console.Write("Digite a placa do veículo: ");
-            placa = Console.ReadLine();
-
-            for (int c = 0; c <= 301; c++)
-            {
-                if (placa == "Váriavel publica da classe do caminhão")
-                {
-                    Console.WriteLine($"Carro da marca: {"Váriavel publica da classe"}\nPlaca: {"Váriavel publica da classe"}\nModelo: {"Váriavel publica da classe"}\nFabricante {"Váriavel publica da classe"}\nAno: {"Váriavel publica da classe"}\nCor: {"Váriavel publica da classe"}\nNúmero de portas: {"Váriavel publica da classe"}\nNúmero de eixos: {"Váriavel publica da classe"}\nPeso máximo: {"Váriavel publica da classe"}\n Váriavel específica: {"Váriavel publica da classe"}");
-                    break;
-                }
-            }
-            for (int c = 0; c <= 301; c++)
-            {
-                if (placa == "Váriavel publica da classe do caminhão")
-                {
-                    Console.WriteLine($"Carro da marca: {"Váriavel publica da classe"}\nPlaca: {"Váriavel publica da classe"}\nModelo: {"Váriavel publica da classe"}\nFabricante {"Váriavel publica da classe"}\nAno: {"Váriavel publica da classe"}\nCor: {"Váriavel publica da classe"}\nNúmero de portas: {"Váriavel publica da classe"}\nCapacidade do porta malas: {"Váriavel publica da classe"}\nBagageiro: {"Váriavel publica da classe"}\n Váriavel específica: {"Váriavel publica da classe"}");
-                    break;
-                }
-            }
-            placa = "placa";
-
-            Console.ReadKey();
-        }
-        
-        
-        //PARTE 4 DO EXERCÍCIO - Consulta por Modelo/Marca
-        static void Caso4()
-        }
             string ModeloMarca = "modelo/marca";
             Console.Write("Digite o modelo/marca do veículo: ");
             ModeloMarca = Console.ReadLine();
@@ -79,6 +50,20 @@ namespace NoiasProject
             ModeloMarca = "modelo/marca";
 
             Console.ReadKey();
+        }
+        
+        //PARTE 7 DO EXERCÍCIO - Exibir todos os caminhões cadastrados
+        static void Caso7()
+        {
+            int VariavelContadoradeCaminhoesCadastrados = 0; 
+            for (int c = 0; c <= 201; c++)
+            {
+                if (VariavelContadoradeCaminhoesCadastrados != 0)
+                {
+                    Console.WriteLine($"Caminhão da marca: {"Váriavel publica da classe"}\nPlaca: {"Váriavel publica da classe"}\nModelo: {"Váriavel publica da classe"}\nFabricante {"Váriavel publica da classe"}\nAno: {"Váriavel publica da classe"}\nCor: {"Váriavel publica da classe"}\nNúmero de portas: {"Váriavel publica da classe"}\nNúmero de eixos: {"Váriavel publica da classe"}\nPeso máximo: {"Váriavel publica da classe"}\n Váriavel específica: {"Váriavel publica da classe"}");
+                    break;
+                }
+            }
         }
     }
 }
