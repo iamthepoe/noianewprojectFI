c7e0fd8b71215b5d2a743a1f1140eff1a3b977f1
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-04-01 08:49:29 -0300

    updating functions and constructors

--- NoiasProject/Program.cs
+++ NoiasProject/Program.cs
@@ -15,6 +15,15 @@ namespace NoiasProject
             string modelo, fabricante, cor;
             Carro[] carro = new Carro[300];
             Caminhao[] caminhao = new Caminhao[200];
+            for(int i = 0; i<carro.Length; i++) //preenchendo os arrays
+            {
+                carro[i] = new Carro("", "", 0000, "", 0, 0, false, false, false);
+            }
+
+            for (int i = 0; i < caminhao.Length; i++) //preenchendo os arrays
+            {
+                caminhao[i] = new Caminhao("", "", 0000,"", 0, 0, 0, 0, false);
+            }
 
             do
             {
@@ -36,12 +45,12 @@ namespace NoiasProject
                         }
                         else
                         {
-            
+
                         }
                         n = 2;
                         Console.Clear();
-                        
-                    break;
+
+                        break;
                     case 2:
                         if (caminhoesRegistrados < 200)
                         {
@@ -59,11 +68,18 @@ namespace NoiasProject
                     case 3:
                         break;
                     case 4:
-                       break;
+                        break;
                     case 5:
                         n = 2;
                         Console.Clear();
                     break;
+
+                    case 6:
+                        ExibirCarros(carro);
+                        break;
+                    case 7:
+                        ExibirCaminhoes(caminhao);
+                        break;
                 };
             } while (n == 2);
             Console.ReadKey();
@@ -84,53 +100,82 @@ namespace NoiasProject
         }
         static Carro CadastrarCarro()
         {
-                Console.Clear();
-                Carro carro = new Carro();
-                Console.WriteLine("Digite o modelo do seu caminhão: ");
-                carro.modelo = Console.ReadLine();
-                Console.WriteLine("Digite o/a fabricante do seu caminhão: ");
-                carro.fabricante = Console.ReadLine();
-                Console.WriteLine("Digite o ano do veículo: ");
-                carro.ano = int.Parse(Console.ReadLine());
-                Console.WriteLine("Digite a cor do seu caminhão: ");
-                carro.cor = Console.ReadLine();
-                Console.WriteLine("Digite o número de portas do seu veículo: ");
-                carro.numero_portas = int.Parse(Console.ReadLine());
-                Console.WriteLine("Qual a capacidade máxima do porta mala: ");
-                carro.capacidadePortaMala = int.Parse(Console.ReadLine());
-                Console.WriteLine("O carro possui bagageiro? [S/N] :");
-                string msg = Console.ReadLine();
-                carro.bagageiro =  msg!="s" || msg!="S" ? true : false;
-                Console.WriteLine("O carro possui cambio? [S/N] :");
-                msg = Console.ReadLine();
-                carro.cambio = msg != "s" || msg != "S" ? true : false;
-                Console.Clear();
-                return carro;
+            Console.Clear();
+            Carro carro = new Carro("", "", 0000, "", 0, 0, false, false, false);
+            Console.WriteLine("Digite o modelo do seu caminhão: ");
+            carro.modelo = Console.ReadLine();
+            Console.WriteLine("Digite o/a fabricante do seu caminhão: ");
+            carro.fabricante = Console.ReadLine();
+            Console.WriteLine("Digite o ano do veículo: ");
+            carro.ano = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a cor do seu caminhão: ");
+            carro.cor = Console.ReadLine();
+            Console.WriteLine("Digite o número de portas do seu veículo: ");
+            carro.numero_portas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a capacidade máxima do porta mala: ");
+            carro.capacidadePortaMala = int.Parse(Console.ReadLine());
+            Console.WriteLine("O carro possui bagageiro? [S/N] :");
+            string msg = Console.ReadLine();
+            carro.bagageiro = msg != "s" || msg != "S" ? true : false;
+            Console.WriteLine("O carro possui cambio? [S/N] :");
+            msg = Console.ReadLine();
+            carro.cambio = msg != "s" || msg != "S" ? true : false;
+            carro.cadastrado = true;
+            Console.Clear();
+
+            return carro;
         }
         static Caminhao CadastrarCaminhao()
         {
-                Console.Clear();
-                Caminhao caminhao = new Caminhao();
-                Console.WriteLine("Digite o modelo do seu caminhão: ");
-                caminhao.modelo = Console.ReadLine();
-                Console.WriteLine("Digite o/a fabricante do seu caminhão: ");
-                caminhao.fabricante = Console.ReadLine();
-                Console.WriteLine("Digite o ano do veículo: ");
-                caminhao.ano = int.Parse(Console.ReadLine());
-                Console.WriteLine("Digite a cor do seu caminhão: ");
-                caminhao.cor = Console.ReadLine();
-                Console.WriteLine("Digite o número de portas do seu veículo: ");
-                caminhao.numero_portas = int.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o número de eixos: ");
-                caminhao.numero_eixos = int.Parse(Console.ReadLine());
-                Console.WriteLine("Digite a carga máxima:");
-                caminhao.maximo_carga = double.Parse(Console.ReadLine());
-                Console.WriteLine("Digite a altura do caminhão ");
-                caminhao.altura = double.Parse(Console.ReadLine());
-                Console.Clear();
-                return caminhao;
+            Console.Clear();
+            Caminhao caminhao = new Caminhao("", "", 0000, "", 0, 0, 0, 0, false);
+            Console.WriteLine("Digite o modelo do seu caminhão: ");
+            caminhao.modelo = Console.ReadLine();
+            Console.WriteLine("Digite o/a fabricante do seu caminhão: ");
+            caminhao.fabricante = Console.ReadLine();
+            Console.WriteLine("Digite o ano do veículo: ");
+            caminhao.ano = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a cor do seu caminhão: ");
+            caminhao.cor = Console.ReadLine();
+            Console.WriteLine("Digite o número de portas do seu veículo: ");
+            caminhao.numero_portas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o número de eixos: ");
+            caminhao.numero_eixos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a carga máxima:");
+            caminhao.maximo_carga = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a altura do caminhão ");
+            caminhao.altura = double.Parse(Console.ReadLine());
+            caminhao.cadastrado = true;
+            Console.Clear();
+            return caminhao;
+        }
+        static void ExibirCarros(Carro[] carros)
+        {
+            Console.Clear();
+            for(int i = 0; i<carros.Length; i++)
+            {
+                if (carros[i].cadastrado)
+                {
+                    carros[i].DadosCarro();
+                }
+            }
+            Console.ReadLine();
+            Console.Clear();
+        }
+        static void ExibirCaminhoes(Caminhao[] caminhoes)
+        {
+            Console.Clear();
+            for (int i = 0; i < caminhoes.Length; i++)
+            {
+                if (caminhoes[i].cadastrado)
+                {
+                    caminhoes[i].DadosCaminhao();
+                }
+            }
+            Console.ReadLine();
+            Console.Clear();
         }
     }
 }
 
-        
+
